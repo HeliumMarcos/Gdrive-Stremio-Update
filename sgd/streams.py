d270be63ba8f52c22935d174c88ef7186ca5835b
Helium Marcos <140931391+HeliumMarcos@users.noreply.github.com>
2026-01-25 00:44:12 -0300

    Atualizar o streams.py

--- sgd/streams.py
+++ sgd/streams.py
@@ -57,7 +57,7 @@ class Streams:
 
         str_format = "🎥;%codec 🌈;%bitDepth;bit 🔊;%audio"
         suffix = self.parsed.get_str(str_format)
-        return f"{file_name}\n💾 {file_size} ☁️ Helium"
+        return f"{file_name}\n💾 {file_size} ☁️ Helium\n{suffix}"
 
     def get_proxy_url(self):
         file_id = self.item.get("id")
